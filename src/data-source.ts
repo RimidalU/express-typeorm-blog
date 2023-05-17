2d8eba9f45aa13cc96de34ab17f55b4aaa36197a
RimidalU <stankevichuuladimiru@gmail.com>
2023-05-18 01:28:31 +0300

    feat: connect to db

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,21 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+console.log(process.env.DB_HOST);
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+	type: "postgres",
+	host: process.env.DB_HOST,
+	port: +process.env.DB_PORT || 5432,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	synchronize: true,
+	logging: false,
+	entities: [User],
+	migrations: [],
+	subscribers: [],
+});
